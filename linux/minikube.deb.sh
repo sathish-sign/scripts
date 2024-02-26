d9d4c7af68d40114d741bbccc6d3c43dedd84706
sathish-sign <112930072+sathish-sign@users.noreply.github.com>
2024-02-26 18:05:27 +0530

    Update minikube.deb.sh
    added -y to install docker components

--- linux/minikube.deb.sh
+++ linux/minikube.deb.sh
@@ -13,7 +13,7 @@ echo \
 sudo apt-get update
 
 # Install the Docker packages.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Verify that the installation is successful by running the hello-world image:
 sudo docker run hello-world
